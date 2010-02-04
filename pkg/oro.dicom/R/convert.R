0f9866f0e3570206ffd00a21d09c214a02de864e
bwhitcher <bwhitcher@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-04 19:02:12 +0000

    Updates to oro.dicom

--- pkg/oro.dicom/R/convert.R
+++ pkg/oro.dicom/R/convert.R
@@ -1,12 +1,45 @@
-str2time <- function(tt) {
+##
+## Copyright (c) 2010, Brandon Whitcher
+## All rights reserved.
+## 
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+## 
+##     * Redistributions of source code must retain the above copyright
+##       notice, this list of conditions and the following disclaimer. 
+##     * Redistributions in binary form must reproduce the above
+##       copyright notice, this list of conditions and the following
+##       disclaimer in the documentation and/or other materials provided
+##       with the distribution.
+##     * Neither the name of Rigorous Analytics Ltd. nor the names of
+##       its contributors may be used to endorse or promote products 
+##       derived from this software without specific prior written 
+##       permission.
+## 
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+##
+## $Id: $
+##
+
+str2time <- function(tt, format.out="%02i:%02i:%08.5f") {
   tt <- as.numeric(tt)
   hh <- as.integer(trunc(tt / 10000))
   tt <- tt %% 10000
   mm <- as.integer(trunc(tt / 100))
   ss <- tt %% 100
-  list(txt = sprintf("%02i:%02i:%08.5f", hh, mm, ss),
-       time = 3600*hh + 60*mm + ss)
+  list(txt = sprintf(format.out, hh, mm, ss), time = 3600*hh + 60*mm + ss)
 }
   
-str2date <- function(dd)
-  format(as.Date(dd, "%Y%m%d"), "%d %b %Y")
+str2date <- function(dd, format.in="%Y%m%d", format.out="%d %b %Y")
+  format(as.Date(dd, format.in), format.out)
