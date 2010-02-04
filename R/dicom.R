dbc03fe0011d1db0241b72a22ce8bee44dd1d8c3
bwhitcher <bwhitcher@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-04 19:02:12 +0000

    Updates to oro.dicom

--- R/dicom.R
+++ R/dicom.R
@@ -1,5 +1,39 @@
+##
+## Copyright (c) 2010, Brandon Whitcher
+## All rights reserved.
+## 
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+## 
+##     * Redistributions of source code must retain the above copyright
+##       notice, this list of conditions and the following disclaimer. 
+##     * Redistributions in binary form must reproduce the above
+##       copyright notice, this list of conditions and the following
+##       disclaimer in the documentation and/or other materials provided
+##       with the distribution.
+##     * Neither the name of Rigorous Analytics Ltd. nor the names of
+##       its contributors may be used to endorse or promote products 
+##       derived from this software without specific prior written 
+##       permission.
+## 
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+##
+## $Id: $
+##
+
 dicomInfo <- function(fname, endian="little", flipud=TRUE, skip128=TRUE,
-                      DICM=TRUE, warn=-1) {
+                      DICM=TRUE, warn=-1, verbose=FALSE) {
   ##
   ## "The default DICOM Transfer Syntax, which shall be supported by
   ## all AEs, uses Little Endian encoding and is specified in Annex
@@ -28,6 +62,81 @@ dicomInfo <- function(fname, endian="little", flipud=TRUE, skip128=TRUE,
   ## +----+----+----+----+----+----+----+----+
   ## |<Group-->|<Element>|<Length----------->|<Value->
 
+  ## Sub-routines
+  
+  unsigned.header <- function(VR, fid, endian) {
+    ## "Unsigned Long" and "Unsigned Short"
+    length <- readBin(fid, integer(), size=2, endian=endian)
+    value <- readBin(fid, integer(), n=length/VR$bytes,
+                     size=VR$bytes, signed=FALSE, endian=endian)
+    list(length=length, value=paste(value, collapse=" "))
+  }
+  
+  signed.header <- function(VR, fid, endian) {
+    ## "Signed Long" and "Signed Short"
+    length <- readBin(fid, integer(), size=2, endian=endian)
+    value <- readBin(fid, integer(), n=length/VR$bytes,
+                     size=VR$bytes, signed=TRUE, endian=endian)
+    list(length=length, value=paste(value, collapse=" "))
+  }
+  
+  floating.header <- function(VR, fid, endian) {
+    ## "Floating Point Single" and "Floating Point Double"
+    length <- readBin(fid, integer(), size=2, endian=endian)
+    value <- readBin(fid, numeric(), n=length/VR$bytes,
+                     size=VR$bytes, signed=TRUE, endian=endian)
+    list(length=length, value=value)
+  }
+  
+  other.header <- function(fid, endian) {
+    skip <- readBin(fid, integer(), size=2, endian=endian)
+    length <- readBin(fid, integer(), size=4, endian=endian)
+    ## value <- iconv(rawToChar(readBin(fid, "raw", length), multiple=TRUE), to="UTF-8")
+    ## list(length=length, value=paste(value, collapse=""))
+    seek(fid, where=seek(fid) + length) # skip over this field
+    list(length=length, value <- "skipped")
+  }
+  
+  sequence.header <- function(fid, endian) {
+    ## "Sequence of Items" with bytes = 0
+    skip <- readBin(fid, integer(), size=2, endian=endian)
+    length <- readBin(fid, integer(), size=4, endian=endian)
+    seek(fid, where=seek(fid) + length) # skip over all sequence items
+    list(length=length, value="sequence")
+  }
+  
+  null.header <- function(VR, group, element, fid, endian) {
+    if (VR$bytes > 0) {
+      if (! implicit) {
+        length <- readBin(fid, integer(), size=2, endian=endian)
+      }
+      ## Trim trailing white space
+      value <- sub(" +$", "",
+                   iconv(rawToChar(readBin(fid, "raw", length)), to="UTF-8"))
+      ## Replace all "\\"s with " "
+      value <- gsub("[\\]", " ", value)
+      ## Remove all non {a-zA-Z} characters with white space
+      ## value <- gsub("[^{a-zA-Z}]", " ", value)
+      if (VR$code == "UI") {
+        value <- sub("\\0", "", value) # Remove trailing \0
+      }
+    } else {
+      if (! implicit) {
+        skip <- readBin(fid, integer(), size=2, endian=endian)
+        length <- readBin(fid, integer(), size=4, endian=endian)
+      }
+      if (length >= 0 &&
+          (VR$code != "SQ" && (group != "FFFE" &&
+             (! element %in% c("E000","E00D","E0DD"))))) {
+        skip <- readBin(fid, integer(), length, size=1, endian=endian)
+        value <- "skip"
+      } else {
+        value <- "nothing matched"
+      }
+    }
+    list(length=length, value=value)
+  }
+  
   ## Warnings?
   oldwarn <- options()$warn
   options(warn=warn)
@@ -38,23 +147,24 @@ dicomInfo <- function(fname, endian="little", flipud=TRUE, skip128=TRUE,
   ## Open connection
   fid <- file(fname, "rb")
   ## First 128 bytes are not used
-  if (skip128)
+  if (skip128) {
     seek(fid, where=128)
+  }
   ## Next four bytes spell "DICM"
   if (DICM) {
-    DICM <- rawToChar(readBin(fid, "raw", n=4))
-    if (DICM != "DICM")
+    if (readChar(fid, n=4) != "DICM") {
       stop("DICM != DICM")
+    }
   }
   hdr <- NULL
   pixel.data <- FALSE
-  while (!pixel.data) {
+  while (! pixel.data) {
     implicit <- FALSE
     group <- dec2hex(readBin(fid, integer(), size=2, endian=endian), 4)
     element <- dec2hex(readBin(fid, integer(), size=2, endian=endian), 4)
     pixel.data <- ifelse(group %in% "7FE0" & element %in% "0010", TRUE, FALSE)
-    vrstr <- rawToChar(readBin(fid, "raw", n=2))
-    if (!any(VRcode %in% vrstr)) {
+    vrstr <- readChar(fid, n=2)
+    if (! vrstr %in% VRcode) {
       ## Implicit VR (see diagram above)
       implicit <- TRUE
       seek(fid, where=seek(fid) - 2) # go back two bytes
@@ -69,63 +179,34 @@ dicomInfo <- function(fname, endian="little", flipud=TRUE, skip128=TRUE,
       VR <- dicom.VRfields()[VRcode %in% "UN", ]
     }
     if (pixel.data) {
-      if (!implicit) { # is this check necessary?
-        skip <- readBin(fid, integer(), size=2, endian=endian)
-        length <- readBin(fid, integer(), size=4, endian=endian)
-      }
-      BitsAllocated <- which(hdr[, 3] %in% "BitsAllocated")
+      skip <- readBin(fid, integer(), size=2, endian=endian)
+      length <- readBin(fid, integer(), size=4, endian=endian)
+      BitsAllocated <- which(hdr[, 3] %in% "BitsAllocated")[1]
       bytes <- as.numeric(hdr[BitsAllocated, 6]) / 8
       ## Assuming only integer() data are being provided
       img <- readBin(fid, integer(), length, size=bytes, endian=endian)
-      value <- NA
+      out <- list(length=length, value=NA)
     } else {
-      if (VR$bytes > 0) {
-        if (!implicit) 
-          length <- readBin(fid, integer(), size=2, endian=endian)
-        if (any(c("UL","US") %in% VR$code)) {
-          ## "Unsigned Long" and "Unsigned Short"
-          value <- readBin(fid, integer(), n=length/VR$bytes,
-                           size=VR$bytes, signed=FALSE, endian=endian)
-          value <- paste(value, collapse=" ")
-        } else {
-          if (any(c("SL","SS") %in% VR$code)) {
-            ## "Signed Long" and "Signed Short"
-            value <- readBin(fid, integer(), n=length/VR$bytes,
-                             size=VR$bytes, signed=TRUE, endian=endian)
-            value <- paste(value, collapse=" ")
-          } else {
-            ## Trim trailing white space
-            value <- sub(" +$", "", rawToChar(readBin(fid, "raw", length)))
-            ## Replace all "\\"s with " "
-            value <- gsub("[\\]", " ", value)
-            ## Remove all non {a-zA-Z} characters with white space
-            ## value <- gsub("[^{a-zA-Z}]", " ", value)
-            if (VR$code %in% "UI")
-              value <- sub("\\0", "", value) # Remove trailing \0
-          }
-        }
-      } else {
-        ## This is an OB, OW, SQ, UN or UT, with 32-bit VL
-        if (!implicit) {
-          skip <- readBin(fid, integer(), size=2, endian=endian)
-          length <- readBin(fid, integer(), size=4, endian=endian)
-        }
-        if (vrstr %in% c("OB","OW")) {
-          value <- paste(rawToChar(readBin(fid, "raw", length), multiple=TRUE), collapse="")
-        } else {
-          if (length >= 0 && (vrstr != "SQ" && (group != "FFFE" && (! element %in% c("E000","E00D","E0DD"))))) {
-            skip <- readBin(fid, integer(), length, size=1, endian=endian)
-            value <- "skip"
-          } else {
-            value <- "nothing matched"
-          }
-        }
-      }
+      out <- switch(VR$code,
+                    UL = unsigned.header(VR, fid, endian),
+                    US = unsigned.header(VR, fid, endian),
+                    SL = signed.header(VR, fid, endian),
+                    SS = signed.header(VR, fid, endian),
+                    FS = floating.header(VR, fid, endian),
+                    FD = floating.header(VR, fid, endian),
+                    OB = other.header(fid, endian),
+                    OW = other.header(fid, endian),
+                    SQ = sequence.header(fid, endian),
+                    null.header(VR, group, element, fid, endian))
     }
-    #cat("", seek(fid), group, element, length, value, sep="\t", fill=TRUE)
     index <- dcm.group %in% group & dcm.element %in% element
     name <- ifelse(any(index), dicom.fields()$name[index], "Unknown")
-    hdr <- rbind(hdr, c(group, element, name, VR$code, length, value))
+    hdr <- rbind(hdr, c(group, element, name, VR$code, out$length,
+                        out$value))
+    if (verbose) {
+      cat("", seek(fid), group, element, name, VR$code, out$length,
+          out$value, sep="\t", fill=TRUE)
+    }
   }
   close(fid)
 
@@ -138,122 +219,102 @@ dicomInfo <- function(fname, endian="little", flipud=TRUE, skip128=TRUE,
   nr <- as.numeric(hdr$value[hdr$name %in% "Rows"])
   nc <- as.numeric(hdr$value[hdr$name %in% "Columns"])
   img <- matrix(img[1:(nc*nr)], nc, nr)
-  if (flipud)
+  if (flipud) {
     img <- img[, nr:1]
+  }
 
   ## Warnings?
-  options(warn = oldwarn)
-  list(hdr = hdr, img = img)
+  options(warn=oldwarn)
+  list(hdr=hdr, img=img)
 }
 
-dicomSeparate <- function(path, debug=TRUE) {
+dicomSeparate <- function(path, debug=TRUE, ...) {
   filenames <- system(paste("ls", path), intern=TRUE)
   nfiles <- length(filenames)
   headers <- images <- vector("list", nfiles)
   names(images) <- names(headers) <- sub("\\.", "", filenames)
   for(i in 1:nfiles) {
-    if(debug && (i %% 100 == 0))
+    if(debug && (i %% 50 == 0))
       cat("  ", i, "files processed...", fill=TRUE)
-    dcm <- dicomInfo(paste(path, filenames[i], sep="/"))
+    dcm <- dicomInfo(paste(path, filenames[i], sep="/"), ...)
     images[[i]] <- dcm$img
     headers[[i]] <- dcm$hdr
   }
   list(hdr = headers, img = images)
 }
 
-dicomToAnalyze <- function(hdr, img) {
-  A <- vector("list")
-  A$size.of.hdr <- as.integer(348)
-  A$endian <- "little"
-  A$extents <- integer(1)
-  A$session.error <- integer(1)
-  ## dim
-  dim <- c(4, nrow(img), ncol(img),
-           ifelse(!is.na(dim(img)[3]), dim(img)[3], 1),
-           ifelse(!is.na(dim(img)[4]), dim(img)[4], 1), 0, 0, 0)
-  A$dim <- as.integer(dim)
-  ## datatype
-  datatype <- switch(hdr$value[hdr$name %in% "BitsAllocated"],
-                     "8" = 4, "16" = 8, "32" = 16, "64" = 64)
-  A$datatype <- as.integer(datatype)
-  ##
-  A$bitpix <- as.integer(hdr$value[hdr$name %in% "BitsAllocated"])
-  A$dim.un0 <- integer(1)
-  ## pixdim
-  pixdim23 <- unlist(strsplit(hdr$value[hdr$name %in% "PixelSpacing"], " "))
-  pixdim4 <- hdr$value[hdr$name %in% "SliceSpacing"]
-  pixdim5 <- hdr$value[hdr$name %in% "RepetitionTime"]
-  A$pixdim <- as.numeric(c(0, pixdim23,
-                           ifelse(!length(pixdim4), 0, pixdim4),
-                           ifelse(!length(pixdim5), 0, pixdim5),
-                           0, 0, 0))
-  ##
-  A$vox.offset <- 0
-  A$cal.max <- 0
-  A$cal.min <- 0
-  A$compressed <- 0
-  A$verified <- 0
-  A$glmax <- A$glmin <- integer(1)
-  A$views <- integer(1)
-  A$vols.added <- integer(1)
-  A$start.field <- integer(1)
-  A$field.skip <- integer(1)
-  A$omax <- A$omin <- A$smax <- A$smin <- integer(1)
-  ## descrip
-  descrip <- hdr$value[hdr$name %in% "SeriesDescription"]
-  if((n <- nchar(descrip)) < 80)
-    A$descrip <- paste(descrip, paste(rep(" ", 80-n-1), collapse=""))
-  else
-    A$descrip <- substring(descrip, 1, 80)
-  ##
-  A$db.type <- paste(rep(" ", 10), collapse="")
-  A$db.name <- paste(rep(" ", 18), collapse="")
-  A$regular <- " "
-  A$hkey.un0 <- " "
-  A$vox.units <- paste(rep(" ", 4), collapse="")
-  A$cal.units <- paste(rep(" ", 8), collapse="")
-  A$aux.file <- paste(rep(" ", 24), collapse="")
-
-  A$orient <- " "
-  ## originator
-  if(any(index <- hdr$name %in% "RequestingPhysician")) {
-    originator <- hdr$value[index]
-    if((n <- nchar(originator)) < 10)
-      A$originator <- paste(originator, paste(rep(" ", 10-n-1), collapse=""))
-    else
-      A$originator <- substring(originator, 1, 10)
+dicom2analyze <- function(img, hdr, descrip="SeriesDescription", ...) {
+  require("dcemriS4")
+  aim <- dcemriS4::anlz(img, ...)
+  ## (x,y) pixel dimensions
+  aim@"pixdim"[2:3] <- as.numeric(unlist(strsplit(extractHeader(hdr, "PixelSpacing", FALSE)[1], " ")))
+  ## z pixel dimensions
+  aim@"pixdim"[4] <- ifelse(aim@"dim_"[1] > 2,
+                            extractHeader(hdr, "SliceThickness")[1],
+                            1)
+  ## description
+  for (i in 1:length(descrip))
+    if (i == 1) {
+      descrip.string <- extractHeader(hdr, descrip[i], FALSE)[1]
+    } else {
+      descrip.string <- paste(descrip.string,
+                              extractHeader(hdr, descrip[i], FALSE)[1],
+                              sep="; ")
+    }
+  if (nchar(descrip.string) > 80) {
+    warning("Description is greater than 80 characters and has been truncated")
+    aim@"descrip" <- substring(descrip.string, 1, 80)
+  } else {
+    aim@"descrip" <- descrip.string
   }
-  else
-    A$originator <- paste(rep(" ", 10), collapse="")
-  ##
-  A$generated <- paste(rep(" ", 10), collapse="")
+  ## originator
+  aim$"originator" <- substring(extractHeader(hdr, "RequestingPhysician")[1],
+                                1, 10)
   ## scannum
-  scannum <- hdr$value[hdr$name %in% "StudyID"]
-  if((n <- nchar(scannum)) < 10)
-    A$scannum <- paste(scannum, paste(rep(" ", 10-n-1), collapse=""))
-  else
-    A$scannum <- substring(scannum, 1, 10)
-  ## patient.id
-  patient.id <- hdr$value[hdr$name %in% "PatientName"]
-  if((n <- nchar(patient.id)) < 10)
-    A$patient.id <- paste(patient.id, paste(rep(" ", 10-n-1), collapse=""))
-  else
-    A$patient.id <- substring(patient.id, 1, 10)
-  ## exp.date
-  exp.date <- hdr$value[hdr$name %in% "StudyDate"]
-  if((n <- nchar(exp.date)) < 10)
-    A$exp.date <- paste(exp.date, paste(rep(" ", 10-n-1), collapse=""))
-  else
-    A$exp.date <- substring(exp.date, 1, 10)
-  ## exp.time
-  exp.time <- hdr$value[hdr$name %in% "StudyTime"]
-  if((n <- nchar(exp.time)) < 10)
-    A$exp.time <- paste(exp.time, paste(rep(" ", 10-n-1), collapse=""))
-  else
-    A$exp.time <- substring(exp.time, 1, 10)
-  ##
-  A$hist.un0 <- paste(rep(" ", 3), collapse="")
-  return(A)
+  aim@"scannum" <- substring(extractHeader(hdr, "StudyID")[1], 1, 10)
+  ## patient_id
+  aim@"patient_id" <- substring(extractHeader(hdr, "PatientID")[1], 1, 10)
+  ## exp_date
+  aim@"exp_date" <- substring(extractHeader(hdr, "StudyDate")[1], 1, 10)
+  ## exp_time
+  aim@"exp_time" <- substring(extractHeader(hdr, "StudyTime")[1], 1, 10)
+  return(aim)
+}
+
+dicom2nifti <- function(img, hdr, units=c("mm","sec"), rescale=FALSE, 
+                        descrip="SeriesDescription", ...) {
+  require("dcemriS4")
+  nim <- dcemriS4::nifti(img, ...)
+  ## (x,y) pixel dimensions
+  nim@"pixdim"[2:3] <- as.numeric(unlist(strsplit(extractHeader(hdr, "PixelSpacing", FALSE)[1], " ")))
+  ## z pixel dimensions
+  nim@"pixdim"[4] <- ifelse(nim@"dim_"[1] > 2,
+                            extractHeader(hdr, "SliceThickness")[1],
+                            1)
+  ## description
+  for (i in 1:length(descrip))
+    if (i == 1) {
+      descrip.string <- extractHeader(hdr, descrip[i], FALSE)[1]
+    } else {
+      descrip.string <- paste(descrip.string,
+                              extractHeader(hdr, descrip[i], FALSE)[1],
+                              sep="; ")
+    }
+  if (nchar(descrip.string) > 80)
+    warning("Description is greater than 80 characters and will be truncated")
+  nim@"descrip" <- descrip.string
+  ## units
+  if (length(units) == 2) {
+    nim@"xyzt_units" <- space.time2xyzt(units[1], units[2])
+  } else {
+    stop("units must be a length=2 vector")
+  }
+  ## rescaling (more of a CT thing?)
+  if (rescale) {
+    nim@"scl_slope" <- extractHeader(hdr, "RescaleSlope")[1]
+    nim@"scl_inter" <- extractHeader(hdr, "RescaleIntercept")[1]
+  }
+  return(nim)
 }
 
 extractHeader <- function(hdrs, string, numeric=TRUE, names=FALSE) {
@@ -266,9 +327,13 @@ extractHeader <- function(hdrs, string, numeric=TRUE, names=FALSE) {
                        })
   out.names <- names(out.list)
   out.vec <- unlist(out.list)
-  if(numeric) {
+  if (numeric) {
     out.vec <- as.numeric(out.vec)
+  }
+  if (names) {
     names(out.vec) <- out.names
+  } else {
+    out.vec <- as.vector(out.vec)
   }
   return(out.vec)
 }
