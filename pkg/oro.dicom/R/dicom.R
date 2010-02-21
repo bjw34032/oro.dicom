0131676b6fccafe801dd0b3569ab90555316534b
bwhitcher <bwhitcher@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-21 10:49:05 +0000

    Fixed path names in dicom.R and streamlining create3D() in create.R

--- pkg/oro.dicom/R/dicom.R
+++ pkg/oro.dicom/R/dicom.R
@@ -239,7 +239,7 @@ dicomSeparate <- function(path, verbose=FALSE, counter=100,
   if (recursive) {
     filenames <- system(paste("find", path, "-type f"), intern=TRUE)
   } else {
-    filenames <- system(paste("ls", path), intern=TRUE)
+    filenames <- file.path(dcm.path, system(paste("ls", path), intern=TRUE))
   }
   if (! is.null(exclude)) {
     filenames <- grep(exclude, filenames, value=TRUE, invert=TRUE)
@@ -251,7 +251,7 @@ dicomSeparate <- function(path, verbose=FALSE, counter=100,
     if (verbose && (i %% counter == 0)) {
       cat("  ", i, "files processed...", fill=TRUE)
     }
-    dcm <- dicomInfo(paste(path, filenames[i], sep="/"), ...)
+    dcm <- dicomInfo(filenames[i], ...)
     images[[i]] <- dcm$img
     headers[[i]] <- dcm$hdr
   }
