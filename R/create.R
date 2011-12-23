2afb4d8380c398d00e145334a3da82cd54c88ab8
bwhitcher <bwhitcher@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-23 21:51:59 +0000

    Renamed functions dicomInfo to readDICOMFile and dicomSeparate to readDICOM

--- R/create.R
+++ R/create.R
@@ -103,8 +103,8 @@ create3D <- function(dcm, mode="integer", transpose=TRUE, pixelData=TRUE,
       }
     } else {
       for (z in 1:Z) {
-        ## img[,,z] <- dicomInfo(names(dcm$hdr)[iop.order[z]])$img
-        img[,,z] <- dicomInfo(names(dcm$hdr)[z])$img
+        ## img[,,z] <- readDICOMFile(names(dcm$hdr)[iop.order[z]])$img
+        img[,,z] <- readDICOMFile(names(dcm$hdr)[z])$img
       }
     }
   }
@@ -185,7 +185,7 @@ create4D <- function(dcm, mode="integer", transpose=TRUE, pixelData=TRUE,
     } else {
       for (w in 1:ntimes) {
         k <- 1
-        dicomInfoImage <- dicomInfo(names(dcm$hdr)[w])$img
+        dicomInfoImage <- readDICOMFile(names(dcm$hdr)[w])$img
         for (i in (X/x):1) {
           for (j in 1:(Y/y)) {
             img[,,k,w] <- dicomInfoImage[((i-1)*x)+1:x, ((j-1)*y)+1:y]
@@ -237,7 +237,7 @@ create4D <- function(dcm, mode="integer", transpose=TRUE, pixelData=TRUE,
       for (z in 1:Z) {
         zz <- (z - 1) %% nslices + 1
         ww <- (z - 1) %/% nslices + 1
-        img[,,zz,ww] <- dicomInfo(names(dcm$hdr)[index[z]])$img
+        img[,,zz,ww] <- readDICOMFile(names(dcm$hdr)[index[z]])$img
       }
     }
   }
