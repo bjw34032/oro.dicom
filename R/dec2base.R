09b673f51523bc9be04675dcfdf216ff03cbdb31
bwhitcher <bwhitcher@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-24 14:42:25 +0000

    The readDICOM() function now accepts a single file as input

--- R/dec2base.R
+++ R/dec2base.R
@@ -1,5 +1,5 @@
 ##
-## Copyright (c) 2010, Brandon Whitcher
+## Copyright (c) 2010, 2011, Brandon Whitcher
 ## All rights reserved.
 ## 
 ## Redistribution and use in source and binary forms, with or without
